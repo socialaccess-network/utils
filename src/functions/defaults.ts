845436c05a7a0e57de9178bd3fc44f34471d8dc1
Micheal Pearce <michealpearce@outlook.com>
2023-11-13 20:07:22 -0500

    update defaults to not mutate original object update package.json to use .d.ts for types

--- src/functions/defaults.ts
+++ src/functions/defaults.ts
@@ -1,13 +1,23 @@
+import { clone, cloneDeep } from './clone'
+import { isDefined } from './isDefined'
+
+/**
+ * Returns a new object including all properties from both objects.
+ *
+ * @param obj The object to check for missing properties
+ * @param defaults The object to use as defaults
+ * @returns a new object including all properties from both objects.
+ */
 export function defaults<T extends object, U extends object>(
 	obj: T,
 	defaults: U,
 ): T & U {
 	return Object.keys(defaults).reduce((acc, key) => {
-		if (obj[key] === undefined) {
+		if (!isDefined(acc[key])) {
 			acc[key] = defaults[key]
 		}
 		return acc
-	}, obj as T & U)
+	}, clone(obj) as T & U)
 }
 
 export function defaultsDeep<T extends object, U extends object>(
@@ -24,5 +34,5 @@ export function defaultsDeep<T extends object, U extends object>(
 			acc[key] = defaultsDeep(obj[key], defaults[key])
 		}
 		return acc
-	}, obj as T & U)
+	}, cloneDeep(obj) as T & U)
 }
